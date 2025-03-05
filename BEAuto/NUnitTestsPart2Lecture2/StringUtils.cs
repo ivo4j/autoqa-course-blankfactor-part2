69274ef7d35e9f06c50d5fb55b280a513d270645
Ivaylo Vasilev <ivo4j@users.noreply.github.com>
2025-03-05 19:31:48 +0200

    Add NUnitTests console app - Lecture 2 - new

---
+++ BEAuto/NUnitTestsPart2Lecture2/StringUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitTestsPart2Lecture2
+{
+    internal class StringUtils
+    {
+
+        public string ReverseString(string input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("The input is null");
+            }
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
